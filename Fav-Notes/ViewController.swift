48d03d90562a7654f249c7e2085c3c2f1ee03e6b
Mc <MacHome@mc-macbook-pro.local>
2016-05-04 21:16:58 -0400

    Initial Commit

---
+++ Fav-Notes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fav-Notes
+//
+//  Created by Mc on 5/4/16.
+//  Copyright © 2016 Good Rock Apps LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
